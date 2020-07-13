1a1719dce4ac82bbaa23a7a6da5d90519271a52c
chensai <chensai@chensaideMacBook-Pro-2.local>
2020-07-13 15:04:18 +0800

    feat:添加请求拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,23 +1,34 @@
+// request 添加拦截器
 import Taro from '@tarojs/taro';
 import { BASE_URL } from '../config/config';
 
 async function taroRequest({ url, method = 'GET', data = {} }) {
-  const res = await Taro.request({
-    url: BASE_URL + url,
-    header: {
-      'Content-Type': 'application/json',
-    },
-    data,
-    method: method.toUpperCase(),
-    mode: 'cors',
-    credentials: 'include',
-    timeout: 6000,
-    dataType: 'json',
-  })
-  // 对结果统一处理
-  const { statusCode, data: resData, errMsg } = res || {};
-  if (statusCode === 200) return resData;
-  throw new Error(`网络请求错误，状态码${statusCode}，${errMsg}`);
+  try {
+    // 对响应结果，添加拦截器chuloi
+    Taro.addInterceptor((chain = {}) => {
+      return chain.proceed(chain.requestParams).then(res => {
+        // 对结果统一处理
+        const { statusCode, data: resData, errMsg } = res || {};
+        if (statusCode === 200) return resData;
+        throw new Error(`网络请求错误，状态码${statusCode}，${errMsg}`);
+      })
+    });
+    const resData = await Taro.request({
+      url: `${BASE_URL}${url}`,
+      header: {
+        'Content-Type': 'application/json',
+      },
+      data,
+      method: method.toUpperCase(),
+      mode: 'cors',
+      credentials: 'include', // 允许跨域共享cookie
+      timeout: 6000,
+      dataType: 'json',
+    })
+    return resData;
+  } catch (error) {
+    console.log('请求发生错误', error);
+  }
 }
 
 export default taroRequest;
